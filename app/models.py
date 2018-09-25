b76155eb5f5f757000e254a6eca427663c6efec7
Grisw <514335188@qq.com>
2018-09-25 10:30:11 +0800

    修复django model account不允许为空的错误

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ import datetime
 
 class Account(models.Model):
     qq = models.CharField(primary_key=True, max_length=20)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=50, null=True, blank=True)
     phone = models.CharField(max_length=11)
     is_lamb = models.BooleanField()
     openid = models.CharField(max_length=32, null=True)
@@ -17,6 +17,6 @@ class Coupon(models.Model):
     sn = models.CharField(primary_key=True, max_length=16)
     lucky_number = models.IntegerField()
     current_count = models.IntegerField(default=0)
-    lamb_account = models.ForeignKey(Account, models.SET_NULL, to_field='qq', null=True, related_name='lamb')
-    lucky_account = models.ForeignKey(Account, models.SET_NULL, to_field='qq', null=True, related_name='lucky')
+    lamb_account = models.ForeignKey(Account, models.SET_NULL, to_field='qq', null=True, blank=True, related_name='lamb')
+    lucky_account = models.ForeignKey(Account, models.SET_NULL, to_field='qq', null=True, blank=True, related_name='lucky')
     amount = models.FloatField(default=0)
