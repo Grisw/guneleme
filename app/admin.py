332051e3e901bede58a32aca67ca3ef9a8a13255
Grisw <514335188@qq.com>
2018-09-26 13:09:12 +0800

    添加admin 日期过滤器。

--- app/admin.py
+++ app/admin.py
@@ -14,7 +14,6 @@ class AccountAdmin(admin.ModelAdmin):
 class CouponAdmin(admin.ModelAdmin):
     list_display = ('sn', 'lucky_number', 'current_count', 'lucky_account', 'amount', 'create_time', 'pick_time')
     readonly_fields = ('amount', 'create_time', 'pick_time')
-    actions = ('delete_old', )
 
     class RemainsFilter(admin.SimpleListFilter):
         title = 'Remains Count'
@@ -34,10 +33,24 @@ class CouponAdmin(admin.ModelAdmin):
                 return queryset.filter(lucky_number=F('current_count'))
             if self.value() == '1':
                 return queryset.filter(lucky_number__gt=F('current_count'))
-    list_filter = (RemainsFilter, 'lucky_account', 'lamb_account')
 
-    def delete_old(self, request, queryset):
-        olds = Coupon.objects.filter(time__lt=datetime.date.today() - datetime.timedelta(days=1))
-        count, rows = olds.delete()
-        self.message_user(request, '%s successfully deleted.' % count)
-    delete_old.short_description = 'Delete coupons created 1 day ago'
+    class CreateTimeFilter(admin.SimpleListFilter):
+        title = 'Create Time'
+        parameter_name = 'create_time'
+
+        def lookups(self, request, model_admin):
+            return (
+                ('-2', 'Before yesterday'),
+                ('-1', 'Yesterday'),
+                ('0', 'Today'),
+            )
+
+        def queryset(self, request, queryset):
+            if self.value() == '-2':
+                return queryset.filter(time__lt=datetime.date.today() - datetime.timedelta(days=1))
+            if self.value() == '-1':
+                return queryset.filter(time__gte=datetime.date.today() - datetime.timedelta(days=1), time__lt=datetime.date.today())
+            if self.value() == '0':
+                return queryset.filter(time__gte=datetime.date.today())
+
+    list_filter = (RemainsFilter, CreateTimeFilter, 'lucky_account', 'lamb_account')
